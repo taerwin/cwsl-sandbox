4aa1b19f540dca7e424eecd033c21f1c58915b16
Damien Irving <irving.damien@gmail.com>
2015-05-12 17:34:43 +1000

    Added little script for getting some example data to work with

---
+++ play_data.sh
@@ -0,0 +1,1 @@
+bash cdo_remap.sh remapcon2 r360x180 /g/data/ua6/drstree/CMIP5/GCM/CSIRO-BOM/ACCESS1-0/historical/mon/ocean/thetao/r1i1p1/thetao_Omon_ACCESS1-0_historical_r1i1p1_199001-199412.nc test.nc
